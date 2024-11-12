93c7f738b7e0003b97f54742a982c9a9e8d01796
yggverse <yggverse@project>
2024-11-12 07:22:11 +0200

    remove extra reference

--- src/line/link.rs
+++ src/line/link.rs
@@ -54,7 +54,7 @@ impl Link {
             // Base resolve not requested
             None => {
                 // Just try convert address to valid URI
-                match Uri::parse(&unresolved_address, UriFlags::NONE) {
+                match Uri::parse(unresolved_address, UriFlags::NONE) {
                     Ok(unresolved_uri) => unresolved_uri,
                     Err(_) => return None,
                 }
