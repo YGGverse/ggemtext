f08ae77b7c16dcbb567e7ed6510b4fa8f306ca6d
yggverse <yggverse@project>
2024-12-03 19:38:39 +0200

    remove extra condition by regex change

--- src/line/code/inline.rs
+++ src/line/code/inline.rs
@@ -12,22 +12,15 @@ impl Inline {
     pub fn from(line: &str) -> Option<Self> {
         // Parse line
         let regex = Regex::split_simple(
-            r"^`{3}([^`]*)`{3}$",
+            r"^`{3}([^`]+)`{3}$",
             line,
             RegexCompileFlags::DEFAULT,
             RegexMatchFlags::DEFAULT,
         );
 
         // Extract formatted value
-        let value = regex.get(1)?.trim();
-
-        if value.is_empty() {
-            return None;
-        }
-
-        // Result
         Some(Self {
-            value: value.to_string(),
+            value: regex.get(1)?.trim().to_string(),
         })
     }
 }
